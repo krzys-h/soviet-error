3e0dfd4fa876ff500e6649ab9ec293610a626516
Krzysztof Zając <krzysztof.zajac2@gmail.com>
2017-08-22 23:45:34 +0200

    Melodies moved to a separate module

---
+++ melody/__init__.py
@@ -0,0 +1,9 @@
+from melody.ussr import USSRMelody
+from melody.internationale import InternationaleMelody
+from melody.wknp import WKNPMelody
+
+melodies = {
+    'USSR': USSRMelody,
+    'Internationale': InternationaleMelody,
+    'WKNP': WKNPMelody,
+}
--- synthesize.py
+++ synthesize.py
@@ -5,6 +5,8 @@ import tempfile
 
 import english_syllable
 
+from melody import melodies
+
 def sing(xml):
     filename = tempfile.mktemp()
 
@@ -12,155 +14,6 @@ def sing(xml):
         f.write(xml)
 
     print('(tts \"%s\" \'singing)' % filename)
-
-
-def get_melody(melody):
-    if melody == 'WKNP':
-        return [
-            ('D4', 0.3),
-            ('B3', 0.3),
-            ('B3', 0.3),
-            ('C4', 0.3),
-            ('A3', 0.3),
-            ('A3', 0.3),
-            ('G3', 0.3),
-            ('B3', 0.3),
-            ('D4', 0.3),
-            ('D4', 0.3),
-            ('B3', 0.3),
-            ('B3', 0.3),
-            ('C4', 0.3),
-            ('A3', 0.3),
-            ('A3', 0.3),
-            ('G3', 0.3),
-            ('B3', 0.3),
-            ('G3', 0.3),
-        ]
-    elif melody == 'INTERN':
-        return [
-            ('D5', 0.5),
-            ('G5', 1.5),
-            ('F#5', 0.5),
-            ('A5', 0.5),
-            ('G5', 0.5),
-            ('D5', 0.5),
-            ('B4', 0.5),
-            ('E5', 2.0),
-            ('C5', 1.5),
-            ('E5', 0.5),
-            ('A5', 1.5),
-            ('G5', 0.5),
-            ('F#5', 0.5),
-            ('E5', 0.5),
-            ('D5', 0.5),
-            ('C5', 0.5),
-            ('B4', 2.0),
-            ('D5', 1.0),
-            ('G5', 1.5),
-            ('F#5', 0.5),
-            ('A5', 0.5),
-            ('G5', 0.5),
-            ('D5', 0.5),
-            ('B4', 0.5),
-            ('E5', 2.0),
-            ('C5', 1.0),
-            ('A5', 0.5),
-            ('G5', 0.5),
-            ('F#5', 1.0),
-            ('A5', 1.0),
-            ('C6', 1.0),
-            ('F#5', 1.0),
-            ('G5', 2.0),
-            ('B5', 0.5),
-            ('A5', 0.5),
-            ('F#5', 2.0),
-            ('E5', 0.5),
-            ('F#5', 0.5),
-            ('G5', 0.5),
-            ('E5', 0.5),
-            ('F#5', 2.0),
-            ('D5', 1.0),
-            ('C#5', 0.5),
-            ('D5', 0.5),
-            ('E5', 1.5),
-            ('A4', 0.5),
-            ('A5', 1.5),
-            ('G5', 0.5),
-            ('F#5', 2.0),
-            ('A5', 0.5),
-            ('A5', 1.5),
-            ('F#5', 0.5),
-            ('D5', 0.5),
-            ('D5', 0.5),
-            ('C#5', 0.5),
-            ('D5', 0.5),
-            ('B5', 2.0),
-            ('G5', 0.5),
-            ('G5', 0.5),
-            ('F#5', 0.5),
-            ('E5', 0.5),
-            ('F#5', 1.0),
-            ('A5', 1.0),
-            ('G5', 1.0),
-            ('E5', 1.0),
-            ('D5', 1.0),
-            ('B5', 0.5),
-            ('A5', 0.5),
-            ('G5', 2.0),
-            ('D5', 1.5),
-        ]    
-    elif melody == 'USSR':
-        return [
-            ('Bb3', 1.0),
-            ('Eb4', 2.0),
-            ('Bb3', 1.5),
-            ('C4', 0.5),
-            ('D4', 2.0),
-            ('G3', 1.0),
-            ('G3', 1.0),
-            ('C4', 2.0),
-            ('Bb3', 1.5),
-            ('Ab3', 0.5),
-            ('Bb3', 2.0),
-            ('Eb3', 1.0),
-            ('Eb3', 1.0),
-            ('F3', 2.0),
-            ('F3', 1.0),
-            ('G3', 1.0),
-            ('Ab3', 2.0),
-            ('Ab3', 1.0),
-            ('Bb3', 1.0),
-            ('C4', 2.0),
-            ('D4', 1.5),
-            ('Eb4', 0.5),
-            ('F4', 3.0),
-            ('Bb3', 1.0),
-            ('G4', 2.0),
-            ('F4', 1.5),
-            ('Eb4', 0.5),
-            ('F4', 2.0),
-            ('D4', 1.0),
-            ('Bb3', 1.0),
-            ('Eb4', 2.0),
-            ('D4', 1.5),
-            ('C4', 0.5),
-            ('D4', 2.0),
-            ('G3', 1.0),
-            ('G3', 1.0),
-            ('C4', 2.0),
-            ('Bb3', 1.5),
-            ('Ab3', 0.5),
-            ('G3', 2.0),
-            ('Bb3', 1.0),
-            ('D4', 1.0),
-            ('Eb4', 2.0),
-            ('D4', 1.5),
-            ('C4', 0.5),
-            ('Bb3', 3.0),
-        ]
-    else:
-        assert(False)
-
 
 
 def calculate_syllabe_number_hotfices():
@@ -181,16 +34,6 @@ def calculate_syllabe_number_hotfices():
     return syllabe_number_hotfices
 
 
-def get_bpm(melody):
-    if melody == 'WKNP':
-        return 30
-    elif melody == 'USSR':
-        return 160
-    elif melody == 'INTERN':
-        return 100
-    else:
-        assert(False)
-
 def synthesize(message, melody):
     syllabe_number_hotfices = calculate_syllabe_number_hotfices()
 
@@ -203,7 +46,7 @@ def synthesize(message, melody):
         '<!DOCTYPE SINGING PUBLIC "-//SINGING//DTD SINGING mark up//EN" ',
         '"Singing.v0_1.dtd"',
         '[]>',
-        '<SINGING BPM="%s">' % get_bpm(melody),
+        '<SINGING BPM="%s">' % melody.get_bpm(),
     ]
 
     def get_first_note():
@@ -211,7 +54,7 @@ def synthesize(message, melody):
         get_first_note.melody = get_first_note.melody[1:]
         return first_note
 
-    get_first_note.melody = get_melody(melody)
+    get_first_note.melody = melody.get_notes()
 
     for digit, digit_text in [
             ('0', 'zero'),
@@ -276,4 +119,4 @@ if __name__ == '__main__':
         print("Usage: %s melody text" % sys.argv[0])
         sys.exit(0)
 
-    synthesize(sys.argv[2], sys.argv[1])
+    synthesize(sys.argv[2], melodies[sys.argv[1]])
